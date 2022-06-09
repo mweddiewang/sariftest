1f02042497c8fe0d9cbcc7d7d738a90f3f7beaa1
mweddiewang <107213053+mweddiewang@users.noreply.github.com>
2022-06-09 14:50:14 -0400

    Add files via upload

---
+++ sarif_test.cpp
@@ -0,0 +1,12 @@
+
+void foo()
+{
+	int arr[4]{};
+	int v = arr[4]; // C6201, C6385 
+	arr[4] = 2;     // C6201, C6386 
+
+	int x;
+	int* py = nullptr;
+	int y = *py;    // C6011 
+	y += x;         // C6001 
+}
